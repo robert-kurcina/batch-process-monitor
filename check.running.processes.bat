e3e1bc76f54eccc2f13373e0c598755eb540e9af
Robert Kurcina <kitrok@gmail.com>
2018-05-09 17:40:35 -0700

    Correctly ignores blank lines
    stopped.processes.txt is deleted if it exists
    stopped.processes.txt is appended for each process which is not running
    
    Need to have this execute every 5 minutes.
    Be sure to check security / permissions

--- check.running.processes.bat
+++ check.running.processes.bat
@@ -21,7 +21,7 @@
 :: this script will execute once per 5 minutes
 ::-------------------------------------------------------------------
 @ECHO OFF
-SETLOCAL enabledelayedexpansion
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 :: script global variables
 SET me=~%n0
@@ -32,47 +32,80 @@ SET me=~%n0
 :GLOBAL_loop
 
 SET ID=0
-
-:: invoke the subroutine function against each process item
-ECHO ---
-ECHO CHECKING processes
-FOR /F "delims=;" %%A IN (sample.monitored.processes.txt) DO (
-    CALL:SUB_checkrunning "%%A"
-)
-
-ECHO ---
-ECHO STOPPED processes
-FOR /L %%n IN (0,1,%ID%) DO (
-   ECHO - !output[%%n]!
-)
-
+CALL:SUB_loadlist
+CALL:SUB_console
+CALL:SUB_writefile
 ENDLOCAL
+
+::-------------------------------------------------------------------
+:: loop routine
+::-------------------------------------------------------------------
+::TIMEOUT 300 /NOBREAK
+::GOTO:GLOBAL_loop
 
 ::-------------------------------------------------------------------
 :: force execution to quit at the end of the "main" logic
 ::-------------------------------------------------------------------
-::TIMEOUT 300 /NOBREAK
-::GOTO:GLOBAL_loop
 EXIT /B %ERRORLEVEL%
+
 
 ::===================================================================
 :: check if tasklist process entry is running
 ::-------------------------------------------------------------------
 :SUB_checkrunning
 
-set processname=%~1%
-tasklist /FI "IMAGENAME eq %processname%" 2>NUL | FIND /I /N "%processname%">NUL
+SET processname=%~1%
+TASKLIST /FI "IMAGENAME eq %processname%" 2>NUL | FIND /I /N "%processname%">NUL
 
 IF "%ERRORLEVEL%" EQU "0" (
-  ECHO %processname% is running!!
+  ECHO - %processname% is RUNNING!!
 )
 
 IF "%ERRORLEVEL%" NEQ "0" (
   SET /A ID=ID+1
-  SET output[%ID%]=%processname%
-  ECHO - %processname% is not running!!
+  SET stopped[%ID%]=%processname%
+  ECHO - %processname% is STOPPED!!
 )
 
 EXIT /B 0
 
-::----------------------------------
+
+
+::===================================================================
+:: invoke the subroutine function against each process item
+::-------------------------------------------------------------------
+:SUB_loadlist
+
+ECHO CHECKING processes
+FOR /F "delims=;" %%A IN (sample.monitored.processes.txt) DO (
+    CALL:SUB_checkrunning "%%A"
+)
+EXIT /B 0
+
+
+::===================================================================
+:: output the stopped list to console
+::-------------------------------------------------------------------
+:SUB_console
+
+ECHO STOPPED processes
+FOR /L %%n IN (1,1,%ID%) DO (
+   ECHO - !stopped[%%n]!
+)
+
+EXIT /B 0
+
+
+::===================================================================
+:: output the stopped list to hard-disk
+::-------------------------------------------------------------------
+:SUB_writefile
+
+ECHO WRITE processes
+IF EXIST stopped.processes.txt DEL stopped.processes.txt
+FOR /L %%n IN (0,1,%ID%) DO (
+  SET str=!stopped[%%n]!
+  IF DEFINED str ECHO !stopped[%%n]! >> stopped.processes.txt
+)
+
+EXIT /B 0
